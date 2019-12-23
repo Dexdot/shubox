bd795ed6a1a0ab23f18e6a82608fb66e45c7c8f9
hvxzcb <dexdot@MacBook-Pro-Kamil.local>
2019-12-24 02:06:09 +0300

    Zoomer mob

--- src/js/init/slider.js
+++ src/js/init/slider.js
@@ -1,5 +1,6 @@
 import Slider from '@/components/slider';
 
+// Gallery
 window.addEventListener('DOMContentLoaded', () => {
   const onUpdate = ({ index, DOM }) => {
     // Dots
@@ -47,5 +48,55 @@ window.addEventListener('DOMContentLoaded', () => {
   });
   $.delegate('.js-gallery-dot', (e, btn) => {
     gallery.update('', $.nodeIndex(btn.parentElement));
+  });
+});
+
+// Gallery
+window.addEventListener('DOMContentLoaded', () => {
+  // const firstNum = $.qs('.zoomer__counter span:first-child');
+  // const lastNum = $.qs('.zoomer__counter span:last-child');
+
+  const onUpdate = ({ index, DOM }) => {
+    // Arrows
+    // firstNum.textContent = index + 1;
+    // lastNum.textContent = DOM.slides.length;
+
+    if (index === 0) {
+      $.each('.js-zooder-prev', el => {
+        el.classList.remove('active');
+      });
+      // firstNum.classList.remove('active');
+    } else {
+      $.each('.js-zooder-prev', el => {
+        el.classList.add('active');
+      });
+      // firstNum.classList.add('active');
+    }
+
+    if (index === DOM.slides.length - 1) {
+      $.each('.js-zooder-next', el => {
+        el.classList.remove('active');
+      });
+      // lastNum.classList.remove('active');
+    } else {
+      $.each('.js-zooder-next', el => {
+        el.classList.add('active');
+      });
+      // lastNum.classList.add('active');
+    }
+  };
+
+  const zoomerMob = new Slider($.qs('.zoomer-mob ul'));
+  onUpdate(window.zoomer.zoomer);
+  window.zoomer.zoomer.DOM.el.addEventListener('slider:updatecomplete', () => {
+    onUpdate(window.zoomer.zoomer);
+    zoomerMob.update('', window.zoomer.zoomer.index);
+  });
+
+  $.delegate('.js-zooder-prev', () => {
+    window.zoomer.prev();
+  });
+  $.delegate('.js-zooder-next', () => {
+    window.zoomer.next();
   });
 });
--- src/js/pages/index.js
+++ src/js/pages/index.js
@@ -3,5 +3,5 @@
 import '@/init/css-props';
 import '@/init/scroll';
 import '@/init/burger';
-import '@/init/slider';
 import '@/init/zoomer';
+import '@/init/slider';
