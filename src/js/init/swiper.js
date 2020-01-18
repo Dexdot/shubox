66bf1d00bff454b26e53d7cc92ae7ddbb6acdf93
Dexdot <dexdotapb@gmail.com>
2020-01-18 16:41:21 +0300

    Reviews fixes

--- src/js/init/swiper.js
+++ src/js/init/swiper.js
@@ -1,7 +1,43 @@
 import Swiper from 'swiper';
 
 if ($.qs('.swiper-container')) {
+  const onUpdate = ({ activeIndex, slides }) => {
+    // Arrows
+    if (activeIndex === 0) {
+      $.each('.js-swiper-prev', el => {
+        el.classList.remove('active');
+      });
+    } else {
+      $.each('.js-swiper-prev', el => {
+        el.classList.add('active');
+      });
+    }
+
+    if (activeIndex === slides.length - 1) {
+      $.each('.js-swiper-next', el => {
+        el.classList.remove('active');
+      });
+    } else {
+      $.each('.js-swiper-next', el => {
+        el.classList.add('active');
+      });
+    }
+  };
+
   const slider = new Swiper('.swiper-container', {
     slidesPerView: 'auto'
+  });
+
+  $.delegate('.js-swiper-prev', () => {
+    slider.slidePrev();
+  });
+
+  $.delegate('.js-swiper-next', () => {
+    slider.slideNext();
+  });
+
+  onUpdate(slider);
+  slider.on('slideChange', () => {
+    onUpdate(slider);
   });
 }
