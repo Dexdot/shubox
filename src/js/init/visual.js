7febfbf5709f6862049904b9f399fa9245ef404c
Dexdot <dexdotapb@gmail.com>
2020-01-28 23:37:40 +0300

    Visual swipes

--- src/js/init/visual.js
+++ src/js/init/visual.js
@@ -1,20 +1,42 @@
-window.addEventListener('DOMContentLoaded', () => {
-  const visualImg = $.qs('.visual-img img');
-  if (!visualImg) return false;
-  // const container = $.qs('.visual .modal__container');
+import Hammer from 'hammerjs';
 
-  // visualImg.addEventListener('load', () => {
-  //   setTimeout(() => {
-  //     container.style.visibility = 'visible';
-  //   }, 200);
-  // });
+window.addEventListener('DOMContentLoaded', () => {
+  const visual = $.qs('.visual-img');
+  if (!visual) return false;
+
+  const visualImg = $.qs('.visual-img img');
+  const sliderList = $.qs('.gallery-slider ul');
+  let i = 0;
+
+  const showByIndex = index => {
+    const slideImg = sliderList.children[index].querySelector('.js-visual');
+    const { src, srcMob } = slideImg.dataset;
+    visualImg.src = window.innerWidth <= 500 ? srcMob : src;
+  };
+
+  const prev = () => {
+    i = i === 0 ? sliderList.children.length - 1 : i - 1;
+    showByIndex(i);
+  };
+
+  const next = () => {
+    i = i === sliderList.children.length - 1 ? 0 : i + 1;
+    showByIndex(i);
+  };
 
   $.delegate('.js-visual', (e, el) => {
-    // container.style.visibility = 'hidden';
+    i = $.nodeIndex(el.closest('li'));
 
     $.qs('[data-modal-open="visual"]').click();
+    showByIndex(i);
+  });
 
-    const { src, srcMob } = el.dataset;
-    visualImg.src = window.innerWidth <= 500 ? srcMob : src;
+  const hammer = new Hammer(visual);
+
+  hammer.on('swipeleft', () => {
+    next();
+  });
+  hammer.on('swiperight', () => {
+    prev();
   });
 });
